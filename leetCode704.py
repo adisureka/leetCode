5f3027f8845553fdd7d9ef908aa8237e4728b780
adisureka <sureka.a@northeastern.edu>
2024-10-28 13:23:53 -0400

    Compared Binary and Linear Search

--- leetCode704.py
+++ leetCode704.py
@@ -1,6 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if(nums[i] == target):
-                return i
-        else: return -1
+
+        # Binary Search [Time Complexity: O(log n)]
+        left = 0
+        right = len(nums) - 1
+        middle =  (left + right) // 2
+        while(left <= right):
+            if(nums[middle] == target):
+                return middle
+            elif(nums[middle] > target):
+                right = middle - 1
+                middle =  (left + right) // 2
+            elif(nums[middle] < target):
+                left = middle + 1
+                middle = (left + right) //  2
+        return - 1
+            
+
+
+        # Linear Search [Time Complexity: O(n)]
+        # for i in range(len(nums)):
+        #     if(nums[i] == target):
+        #         return i
+        # else: return -1
